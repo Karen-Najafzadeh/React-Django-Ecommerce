7c56c0f080c1fd3402c948a883a7dc249c624e86
Karen <najafzadehkaren@gmail.com>
2024-03-17 14:02:40 +0330

    customized admin for shop app

--- server/shop/admin.py
+++ server/shop/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from shop.models import *
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdminAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','in_stock','price','category','sub_category','likes']
+    list_editable = ['price','likes','in_stock']
+    list_filter = ['category','sub_category']
+    list_per_page = 20
+    search_fields = ['name']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    search_fields = ['user']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):   
+    list_display = ['user','product_id']
+    list_filter = ['product_id']
+    search_fields = ['user','message']
+
