fe5fdf5de498bdc09401aed2264f1c2151a267a1
ari7hkm <arianhakim90@gmail.com>
2024-03-17 14:31:00 +0330

    Model.py Updated

--- server/shop/models.py
+++ server/shop/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import MinValueValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profiles/image', null=True, blank=True)
-    favorites = models.ManyToManyField('Product')
+    favorites = models.ManyToManyField('Product', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
 
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
 
 
 
@@ -38,10 +56,7 @@ class Product(models.Model):
     
     name = models.CharField(max_length=100)
     description = models.TextField()
-    in_stock = models.CharField(
-        max_length=1, choices=PRODUCT_STATUS_CHOICES, 
-        default=PRODUCT_STATUS_IN_STOCK
-    )
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     sub_category = models.CharField(
@@ -49,6 +64,10 @@ class Product(models.Model):
     )
     image = models.ImageField(upload_to='products/image')
     likes = models.PositiveIntegerField()
+
+
+    def __str__(self):
+        return self.name
 
 
 
